7af91112b61b5ea51f1561e0895538a2b670a0bc
Sudarshan04 <sudarshanrajan2017@gmail.com>
2023-11-14 23:18:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function validateForm() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm-password").value;
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Invalid email address");
+        return;
+    }
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters long");
+        return;
+    }
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return;
+    }
+    alert("Form submitted successfully");
+}
